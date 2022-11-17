5bbef827c2bbca572a9a639e1e03e51354aaf0d8
Tiago <tiago_barbosa-silva@hotmail.com>
2022-11-17 00:30:48 -0300

    v3 Login/Cad

--- src/main/java/br/com/fiap/fintech/controller/LoginServlet.java
+++ src/main/java/br/com/fiap/fintech/controller/LoginServlet.java
@@ -2,9 +2,16 @@ package br.com.fiap.fintech.controller;
 
 import java.io.IOException;
 
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import br.com.fiap.fintech.Dao.DAOFactory;
+import br.com.fiap.fintech.Dao.ContaUsuario.ContaUsuarioDao;
 import br.com.fiap.fintech.entity.ContaUsuario;
-import br.com.fiap.fintech.entity.Movimentacao;
 import br.com.fiap.fintech.util.Function;
 
 
@@ -15,28 +22,29 @@ import br.com.fiap.fintech.util.Function;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		dao = DAOFactory.getContaDAO();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		try{
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");	
+			
+			if(email == "emailBanco") {
+				
+			}
+			
+		}catch(DBException db) {
+			db.printStackTrace();
+			request.setAttribute("erro", "Erro ao entrar");
+		}catch(Exception e){
+			e.printStackTrace();
+			request.setAttribute("erro","Por favor, valide os dados");
+		}
+		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
-
 }
